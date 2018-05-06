ec6478018909fdce24272a810ee346202d47a268
Benoit DAVID <benoit.davi@free.fr>
2018-05-06 08:38:24 +0000

    gestion fragment

--- index.php
+++ index.php
@@ -4,9 +4,20 @@ name: index.php
 title: index.php - version 2 du visualiseur de documents Yaml
 doc: |
   version minimum:
-    - pas d'affichage d'un fragment
+    - pas d'affichage des textes en markdown
     - pas de gestion des utilisateurs ni de gestion du droit de modification d'un document
+
+  Le script utilise les 2 variables de session suivantes:
+    - docuids : liste des docuids du chemin des documents sous la forme de 
+      - le premier est normalement le catalogue d'accueil
+      - le dernier est normalement le document courant
+      - les documents intermédiaires sont normalemnt des catalogues intermédiaires
+    - homeCatalog : docuid du catalogue d'accueil ou absent
+  En mode anonyme, homeCatalogue n'est pas défini. docuids contient qd même l'historique des documents
+
 journal: |
+  1-4/5/2018:
+  - améliorations
   30/4/2018:
   - restructuration
 */
@@ -14,10 +25,42 @@ session_start();
 require_once __DIR__.'/../spyc/spyc2.inc.php';
 require_once __DIR__.'/yd.inc.php';
 
+// Affichage du menu et du fil d'ariane
+// Le menu et le fil d'ariane doivent être affichés après avoir géré $_SESSION['docuids']
+function show_menu(array $docuids) {
+  // affichage du menu
+  $ypath = isset($_GET['ypath']) ? $_GET['ypath']: '';
+  echo "<table border=1><tr><td><b>Menu :</b></td>\n";
+  if ($docuids)
+    echo "<td><a href='?action=show",($ypath?'&amp;ypath='.urlencode($ypath):''),"'>show</a></td>\n";
+  if ($docuids)
+    echo "<td><a href='?action=edit'>edit</a></td>\n";
+  if (count($docuids) > 1)
+    echo "<td><a href='?action=clone'>clone</a></td>\n";
+  echo "<td><a href='?action=dump",($ypath?'&amp;ypath='.urlencode($ypath):''),"'>dump</a></td>\n";
+  echo "<td><a href='?action=unset'>unset</a></td>\n";
+  echo "</tr></table>\n";
+
+  // affichage du fil d'ariane
+  if ($docuids) {
+    echo "<form>\n";
+    $nbre = count($docuids);
+    foreach ($docuids as $i => $docuid) {
+      if ($i < $nbre-1)
+        echo "<a href='?action=nav&amp;doc=$docuid'>&gt;</a> ";
+      else
+        echo "<b>*</b>";
+    }
+    echo "&nbsp;<input type='text' name='ypath' size=80 value='$ypath'></form>\n";
+    echo "<br>\n";
+  }
+}
+
 echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>yaml</title></head><body>\n";
 
 // action edit - génération du formulaire d'édition du document courant
 if (isset($_GET['action']) and ($_GET['action']=='edit')) {
+  show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
   if (!isset($_SESSION['docuids'])) {
     echo "<b>Erreur: aucun document courant</b>\n";
   }
@@ -26,7 +69,7 @@ if (isset($_GET['action']) and ($_GET['action']=='edit')) {
     $text = ydread($docuid);
     echo "<table><form action='?action=store' method='post'>\n",
          "<tr><td><textarea name='text' rows='40' cols='80'>$text</textarea></td></tr>\n",
-         "<tr><td><input type='submit' value='Envoyer'></td></tr>\n",
+         "<tr><td><input type='submit' value='Enregistrer'></td></tr>\n",
          "</form></table>\n";
     die();
   }
@@ -35,16 +78,23 @@ if (isset($_GET['action']) and ($_GET['action']=='edit')) {
 // action store - enregistrement d'un contenu à la suite d'une édition
 // on pourrait renvoyer une édition lorsque le contenu n'est pas conforme Yaml
 if (isset($_GET['action']) and ($_GET['action']=='store')) {
+  show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
   if (!isset($_SESSION['docuids'])) {
-    echo "<b>Erreur: aucun document courant</b>\n";
+    echo "<b>Erreur: aucun document courant</b><br>\n";
   }
   else {
     $docuid = $_SESSION['docuids'][count($_SESSION['docuids'])-1];
     ydwrite($docuid, $_POST['text']);
     echo "Enregistrement du document $docuid<br>\n";
     $doc = new_yamlDoc($docuid, $_POST['text']);
-    $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : null;
-    $doc->show($ypath);
+    if (!$doc) {
+      echo "<b>Erreur: le document $docuid ne correspond pas à un YamlDoc</b>\n";
+      echo "<h2>doc $docuid</h2><pre>\n$_POST[text]\n</pre>\n";
+    }
+    else {
+      $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : '';
+      $doc->show($ypath);
+    }
   }
 }
 
@@ -52,11 +102,12 @@ if (isset($_GET['action']) and ($_GET['action']=='store')) {
 // gestion des variables de session et affichage de ce document
 if ((!isset($_GET['action']) or ($_GET['action']=='nav')) and isset($_GET['doc'])) {
   if (($text = ydread($_GET['doc'])) === FALSE) {
-    echo "<b>Erreur le fichier $_GET[doc] n'existe pas</b>\n";
+    show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
+    echo "<b>Erreur: le document $_GET[doc] n'existe pas</b>\n";
   }
   else {
     $doc = new_yamlDoc($_GET['doc'], $text);
-    if ($homeCatalog = $doc->isHomeCatalog()) {
+    if ($doc and ($homeCatalog = $doc->isHomeCatalog()) ){
       $_SESSION['homeCatalog'] = $homeCatalog;
       $_SESSION['docuids'] = [ $_GET['doc'] ];
     }
@@ -66,63 +117,86 @@ if ((!isset($_GET['action']) or ($_GET['action']=='nav')) and isset($_GET['doc']
       $_SESSION['docuids'][] = $_GET['doc'];
     else
       $_SESSION['docuids'] = array_slice($_SESSION['docuids'], 0, $k+1);
-    $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : null;
-    $doc->show($ypath);
+    show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
+    if ($doc) {
+      $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : '';
+      $doc->show($ypath);
+    }
+    else {
+      echo "<b>Erreur: le document $_GET[doc] ne correspond pas à un YamlDoc</b><br>\n";
+      echo "<h2>doc $_GET[doc]</h2><pre>\n$text\n</pre>\n";
+    }
   }
 }
 
 // action show - lecture et affichage du document courant
 if ((!isset($_GET['action']) or ($_GET['action']=='show')) and !isset($_GET['doc'])) {
+  show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
   if (!isset($_SESSION['docuids'])) {
-    echo "<b>Erreur: il n'y a pas de document courant</b>\n";
+    echo "<b>Erreur: il n'y a pas de document courant</b><br>\n";
   }
   else {
     $docuid = $_SESSION['docuids'][count($_SESSION['docuids'])-1];
     if (($text = ydread($docuid)) === FALSE) {
-      echo "<b>Erreur: le fichier $docuid n'existe pas</b>\n";
+      echo "<b>Erreur: le document $docuid n'existe pas</b><br>\n";
+    }
+    elseif (!($doc = new_yamlDoc($docuid, $text))) {
+      echo "<b>Erreur: le document $docuid ne correspond pas à un YamlDoc</b>\n";
+      echo "<h2>doc $docuid</h2><pre>\n$text\n</pre>\n";
     }
     else {
-      $doc = new_yamlDoc($docuid, $text);
-      $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : null;
+      $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : '';
       $doc->show($ypath);
     }
   }
 }
 
+// action clone
+if (isset($_GET['action']) and ($_GET['action']=='clone')) {
+  if (!isset($_SESSION['docuids'])) {
+    show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
+    echo "<b>Erreur: il n'y a pas de document courant</b>\n";
+  }
+  elseif (count($_SESSION['docuids']) <= 1) {
+    show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
+    echo "<b>Erreur: il n'y a pas de catalogue parent</b>\n";
+  } else {
+    $docuid = array_pop($_SESSION['docuids']);
+    $catuid = $_SESSION['docuids'][count($_SESSION['docuids'])-1];
+    $newdocuid = uniqid();
+    $_SESSION['docuids'][] = $newdocuid;
+    YamlCatalog::store_in_catalog($newdocuid, $catuid);
+    ydwrite($newdocuid, ydread($docuid));
+    show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
+    echo "contenu enregistré dans $newdocuid<br>\n";
+  }
+}
+
 // action dump - affichage des variables de session et du document courant
 if (isset($_GET['action']) and ($_GET['action']=='dump')) {
+  show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
   echo "<pre>";
   echo "_SESSION = "; print_r($_SESSION);
   if (isset($_SESSION['docuids'])) {
     $docuid = $_SESSION['docuids'][count($_SESSION['docuids'])-1];
-    echo "<h2>doc $docuid</h2>\n",ydread($docuid);
+    $ypath = isset($_GET['ypath']) ? $_GET['ypath'] : '';
+    $text = ydread($docuid);
+    echo "<h2>doc $docuid</h2>\n";
+    if (!$ypath)
+      echo $text;
+    $doc = new_yamlDoc($docuid, $text);
+    if ($ypath) {
+      echo "ypath=$ypath\n";
+      $doc->displayText($ypath);
+    }
+    echo "<h2>var_dump</h2>\n"; $doc->dump($ypath);
   }
   echo "</pre>\n";
 }
 
 // action unset - effacement des variables de session
 if (isset($_GET['action']) and ($_GET['action']=='unset')) {
+  show_menu(isset($_SESSION['docuids']) ? $_SESSION['docuids'] : []);
   foreach ($_SESSION as $key => $value)
     unset($_SESSION[$key]);
-}
-
-// Affichage du fil d'ariane
-if (isset($_SESSION['docuids'])) {
-  $nbre = count($_SESSION['docuids']);
-  foreach ($_SESSION['docuids'] as $i => $docuid) {
-    if ($i < $nbre-1)
-      echo "<a href='?action=nav&amp;doc=$docuid'>&gt;</a> ";
-    else
-      echo "<b>*</b>";
-  }
-}
-
-// Affichage du menu
-if (1) {
-  echo "<h2>Menu</h2><ul>\n";
-  echo "<li><a href='?action=show'>show</a>\n";
-  echo "<li><a href='?action=edit'>edit</a>\n";
-  echo "<li><a href='?action=dump'>dump</a>\n";
-  echo "<li><a href='?action=unset'>unset</a>\n";
-  echo "</ul>\n";
 }
--- yd.inc.php
+++ yd.inc.php
@@ -3,7 +3,15 @@
 name: yd.inc.php
 title: yd.inc.php - fonctions générales pour yamldoc
 doc: |
+  ne marche pas
+    doc=baseadmin
+    ypath=/tables/name=regionmetro/data/code=84/code,title,(json-ld/geo),(départements/(code,title))
 journal: |
+  6/5/2018:
+  - ajout de sort
+  3-4/5/2018:
+  - traitement de ypath
+  - y.c. traiter des requêtes du type: ypath=/data/title,(json-ld/geo/box)
   1/5/2018:
   - refonte pour index.php v2
   19/4/2018:
@@ -23,18 +31,48 @@ function ydread(string $uid) {
   return @file_get_contents("docs/$uid.yaml");
 }
 
-// le paramètre est-il une liste d'atomes ?
-function is_listOfAtoms($list) {
-  // ce doit être un array
+// fonction de comparaison utilisée dans le tri d'un tableau
+$keys_for_sort = [];
+function cmp(array $a, array $b) {
+  global $keys_for_sort;
+  $key = $keys_for_sort[0];
+  $asc = true;
+  if (!isset($a[$key]) and !isset($b[$key]))
+    return 0;
+  // une valeur indéfinie est inférieure à une valeur définie
+  if (!isset($a[$key]))
+    return $asc ? -1 : 1;
+  if (!isset($b[$key]))
+    return $asc ? 1 : -1;
+  
+  if ($a[$key] == $b[$key])
+    return 0;
+  if ($asc)
+    return ($a[$key] < $b[$key]) ? -1 : 1;
+  else
+    return ($a[$key] < $b[$key]) ? 1 : -1;
+}
+
+
+// le paramètre est-il une liste ?
+// une liste est un array pour lequel les clés sont une liste des entiers s'incrémentant de 1 à partir de 0
+function is_list($list) {
   if (!is_array($list))
     return false;
-  // les clés doivent être la liste des entiers à partir de 0
   foreach (array_keys($list) as $k => $v)
     if ($k !== $v)
       return false;
+  return true;
+}
+
+// le paramètre est-il une liste d'atomes, y.c. list(list), ... ?
+function is_listOfAtoms($list) {
+  // ce doit être un array et une liste
+  if (!is_array($list) or !is_list($list))
+    return false;
   // aucun des atom ne doit être un array
-  foreach ($list as $atom)
-    if (is_array($atom))
+  foreach ($list as $elt)
+    if (is_array($elt) and !is_list($elt))
       return false;
   return true;
 }
@@ -48,30 +86,31 @@ function is_listOfTuples($list) {
   return $ret;
 }
 function is_listOfTuples_i($list) {
-  // ce doit être un array
-  if (!is_array($list))
+  // ce doit être un array et une liste
+  if (!is_array($list) or !is_list($list))
     return false;
-  // les clés doivent être la liste des entiers à partir de 0
-  foreach (array_keys($list) as $k => $v)
-    if ($k !== $v)
-      return false;
-  // chaque tuple doit être un array
+  // chaque tuple doit être un array et pas une liste
   foreach ($list as $tuple)
-    if (!is_array($tuple))
+    if (!is_array($tuple) or is_list($tuple))
       return false;
   return true;
 }
 
-// affichage d'une liste d'atomes comme <ul><li>
-function showListOfAtoms(array $list, string $prefix='') {
-  echo "<ul>";
-  foreach ($list as $atom)
-    echo "<li>$atom\n";
+// affichage d'une liste d'atomes, ou list(list) ... comme <ul><li>
+function showListOfAtoms(array $list, string $prefix) {
+  echo "<ul>\n";
+  foreach ($list as $i => $elt) {
+    echo "<li>";
+    if (is_array($elt))
+      showListOfAtoms($elt, "$prefix/$i");
+    else
+      echo "$elt\n";
+  }
   echo "</ul>\n";
 }
 
 // affichage d'une liste de tuples comme table Html
-function showListOfTuplesAsTable(array $table, string $prefix='') {
+function showListOfTuplesAsTable(array $table, string $prefix) {
   $keys = []; // liste des clés d'au moins un tuple
   //echo "<pre>"; print_r($tab); echo "</pre>\n";
   foreach ($table as $tuple) {
@@ -80,7 +119,7 @@ function showListOfTuplesAsTable(array $table, string $prefix='') {
         $keys[] = $key;
     }
   }
-  echo "<pre>"; print_r($keys); echo "</pre>\n";
+  //echo "<pre>keys = "; print_r($keys); echo "</pre>\n";
 
   echo "<table border=1>\n";
   foreach ($keys as $key)
@@ -93,23 +132,11 @@ function showListOfTuplesAsTable(array $table, string $prefix='') {
         echo "<td></td>";
       elseif (is_numeric($tuple[$key]))
         echo "<td align='right'>",$tuple[$key],"</td>";
-      elseif (is_listOfAtoms($tuple[$key])) {
+      else {
         echo "<td>";
-        showListOfAtoms($tuple[$key], "$prefix/$key");
+        showSomething($tuple[$key], "$prefix/$key");
         echo "</td>";
       }
-      elseif (is_listOfTuples($tuple[$key])) {
-        echo "<td>";
-        showListOfTuplesAsTable($tuple[$key], "$prefix/$key");
-        echo "</td>";
-      }
-      elseif (is_array($tuple[$key])) {
-        echo "<td>";
-        showArrayAsTable($tuple[$key], "$prefix/$key");
-        echo "</td>";
-      }
-      else
-        echo "<td>",$tuple[$key],"</td>";
     }
     echo "</tr>\n";
   }
@@ -117,23 +144,30 @@ function showListOfTuplesAsTable(array $table, string $prefix='') {
 }
 
 // affichage d'un array comme table Html
-function showArrayAsTable(array $data, string $prefix='') {
+function showArrayAsTable(array $data, string $prefix) {
   echo "<table border=1>\n";
   foreach ($data as $key => $value) {
     echo "<tr><td>$key</td><td>\n";
-    if (is_listOfAtoms($value))
-      showListOfAtoms($value, "$prefix/$key");
-    elseif (is_listOfTuples($value))
-      showListOfTuplesAsTable($value, "$prefix/$key");
-    elseif (is_array($value))
-      showArrayAsTable($value, "$prefix/$key");
-    elseif (is_string($value) and (strpos($value, "\n")!==FALSE))
-      echo "<pre>$value</pre>";
-    else
-      echo $value;
+    showSomething($value, "$prefix/$key");
     echo "</td></tr>\n";
   }
   echo "</table>\n";
+}
+
+// aiguille l'affichage en fonction du type du paramètre
+function showSomething($data, string $prefix='') {
+  if (is_listOfAtoms($data))
+    showListOfAtoms($data, $prefix);
+  elseif (is_listOfTuples($data))
+    showListOfTuplesAsTable($data, $prefix);
+  elseif (is_array($data))
+    showArrayAsTable($data, $prefix);
+  elseif (is_string($data) and (strpos($data, "\n")!==FALSE))
+    echo "<pre>$data</pre>";
+  elseif (is_null($data))
+    echo 'null';
+  else
+    echo $data;
 }
 
 // crée un YamlDoc à partir d'un texte
@@ -147,7 +181,7 @@ function new_yamlDoc(string $docuid, string $text) {
     if (class_exists($yamlClass))
       return new $yamlClass ($docuid, $data);
     else
-      echo "<b>Erreur: la classe $yamlClass n'est pas définie</b>\n";
+      echo "<b>Erreur: la classe $yamlClass n'est pas définie</b><br>\n";
   }
   return new YamlDoc($docuid, $data);
 }
@@ -160,9 +194,142 @@ class YamlDoc {
   function __construct(string $docuid, array $data) { $this->docuid = $docuid;  $this->data = $data; }
   function isHomeCatalog() { return null; }
   
-  function show(?string $ypath) {
+  // affiche le doc ou le fragment si ypath est non vide
+  function show(string $ypath) {
     //echo "<pre>"; print_r($this->data); echo "</pre>\n";
-    showArrayAsTable($this->data);
+    showSomething(self::sextract($this->data, $ypath));
+  }
+  
+  function dump(string $ypath) {
+    var_dump(self::sextract($this->data, $ypath));
+  }
+  
+  // génère le texte correspondant au fragment défini par ypath
+  // améliore la sortie en supprimant les débuts de ligne
+  function displayText(string $ypath) {
+    $fragment = self::sextract($this->data, $ypath);
+    $text = spycDump($fragment);
+    $pattern = '!(\n +- )\n +!';
+    while (preg_match($pattern, $text, $matches)) {
+      $text = preg_replace($pattern, $matches[1], $text, 1);
+    }
+    echo $text;
+  }
+  
+  
+  // extrait le premier elt de ypath
+  static function extract_ypath(string $ypath): string {
+    if (substr($ypath,0,1)=='/')
+      $ypath = substr($ypath,1);
+    $prof = 0;
+    for ($i=0; $i<strlen($ypath); $i++) {
+      $c = substr($ypath, $i, 1);
+      if (($c=='/') and ($prof==0))
+        return substr($ypath, 0, $i);
+      elseif ($c=='(')
+        $prof++;
+      elseif ($c==')')
+        $prof--;
+    }
+    return $ypath;
+  }
+  
+  // retourne le sous-document défini par path qui est une chaine
+  function extract(string $ypath) {
+    return self::sextract($this->data, $ypath);
+  }
+  
+  // retourne le fragment défini par la chaine ypath
+  static function sextract(array $data, string $ypath) {
+    //echo "extract(ypath=$ypath)<br>\n";
+    if (!$ypath)
+      return $data;
+    echo "ypath=$ypath<br>\n";
+    $elt = self::extract_ypath($ypath);
+    $ypath = substr($ypath, strlen($elt)+1);
+    echo "elt=$elt<br>\n";
+    if (strpos($elt,'=') !== false) {
+      $query = explode('=', $elt);
+      $data = self::select($data, $query[0], $query[1]);
+    }
+    elseif (preg_match('!^sort\(([^)]+)\)$!', $elt, $matches))
+      $data = self::sort($data, $matches[1]);
+    else
+      $data = self::project($data, $elt);
+    if (!$ypath)
+      return $data;
+    if (is_array($data))
+      return self::sextract($data, $ypath);
+    else
+      return null;
+  }
+  
+  // selection dans la liste de tuples $data sur $key=$value
+  static function select(array $data, string $key, string $value) {
+    $result = [];
+    foreach ($data as $tuple)
+      if ($tuple[$key]==$value)
+        $result[] = $tuple;
+    if (count($result)==0)
+      return null;
+    elseif (count($result)==1)
+      return $result[0];
+    else
+      return $result;
+  }
+  
+  // projection de $data sur $keys
+  static function project(array $data, string $keys) {
+    $keys = explode(',', $keys);
+    if (is_listOfTuples($data)) {
+      $result = [];
+      foreach ($data as $tuple) {
+        if (count($keys)==1) {
+          $result[] = $tuple[$keys[0]];
+        }
+        else {
+          $t = [];
+          foreach ($keys as $key) {
+            if (substr($key,0,1)=='(') {
+              $ypath = substr($key, 1, strlen($key)-2);
+              $skeys = explode('/', $ypath);
+              $skey = $skeys[count($skeys)-1];
+              $t[$skey] = self::sextract($tuple, $ypath);
+            }
+            else
+              $t[$key] = $tuple[$key];
+          }
+          $result[] = $t;
+        }
+      }
+      return $result;
+    }
+    elseif (count($keys)==1)
+      return $data[$keys[0]];
+    else {
+      $t = [];
+      foreach ($keys as $key) {
+        if (substr($key,0,1)=='(') {
+          $ypath = substr($key, 1, strlen($key)-2);
+          $skeys = explode('/', $ypath);
+          $skey = $skeys[count($skeys)-1];
+          if (in_array($skey, $keys))
+            $skey = $ypath;
+          $t[$skey] = self::sextract($data, $ypath);
+        }
+        else
+          $t[$key] = $data[$key];
+      }
+      return $t;
+    }
+  }
+  
+  // tri de $data sur $keys
+  static function sort(array $data, string $keys) {
+    global $keys_for_sort;
+    $keys_for_sort = explode(',', $keys);
+    usort($data, 'cmp');
+    return $data;
   }
 };
 
@@ -177,6 +344,14 @@ class YamlCatalog extends YamlDoc {
     }
     echo "</ul>\n";
   }
+  
+  // ajoute un doc dans un catalogue
+  static function store_in_catalog(string $docuid, string $catuid) {
+    $contents = spycLoadString(ydread($catuid));
+    //print_r($contents);
+    $contents['contents'][$docuid] = ['title'=> "document $docuid" ];
+    ydwrite($catuid, spycDump($contents));
+  }
 };
 
 // classe des catalogues d'accueil
@@ -184,20 +359,6 @@ class YamlHomeCatalog extends YamlCatalog {
   function isHomeCatalog() { return $this->docuid; }
 };
 
-/*
-// retourne le sous-document défini par path qui est une chaine ou un array de clés
-function ypath(array $data, $path) {
-  if (!$path)
-    return $data;
-  if (is_string($path)) {
-    $path = explode('/', $path);
-    if (!$path[0])
-      array_shift($path);
-  }
-  $key = array_shift($path);
-  return ypath($data[$key], $path);
-}
-*/
 
 if (basename(__FILE__)<>basename($_SERVER['PHP_SELF'])) return;
 
