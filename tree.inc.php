650df224b15166c9caf8905986e506d65cb6d3a4
Benoit DAVID <benoit.davi@free.fr>
2018-05-13 21:15:21 +0000

    13mai

---
+++ tree.inc.php
@@ -0,0 +1,28 @@
+<?php
+/*PhpDoc:
+name: tree.inc.php
+title: tree.inc.php - classe Tree
+doc: |
+journal: |
+  13/5/2018:
+  - création
+*/
+
+// classe Tree
+class Tree extends YamlDoc {
+  
+  static function showTree(array $children) {
+    echo "<ul>\n";
+    foreach ($children as $child) {
+      echo "<li>$child[t]\n";
+      if (isset($child['c']))
+        self::showTree($child['c']);
+    }
+    echo "</ul>\n";
+  }
+  
+  function show(string $ypath) {
+    echo "<h1>",$this->data['title'],"</h1>\n";
+    self::showTree($this->data['children']);
+  }
+};
