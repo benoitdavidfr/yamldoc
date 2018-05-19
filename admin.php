6b946eb640c77f192916a5cd4be8148cb192b1cc
Benoit DAVID <benoit.davi@free.fr>
2018-05-19 10:58:58 +0000

    ajout stats dans admin

--- admin.php
+++ admin.php
@@ -14,25 +14,46 @@ require_once __DIR__.'/yd.inc.php';
 
 echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>admin</title></head><body>\n";
 
-$docs = []; // [ docid=> [ 'doc'=> objet doc, 'catalogs'?=> [ catalogid ], 'shown'?=> 1 ] ]
-$wd = opendir('docs');
-while (false !== ($entry = readdir($wd))) {
-  if (!preg_match('!^(.*)\.(yaml|php)$!', $entry, $matches))
-    continue;
-  //echo "$entry<br>\n";
-  $docid = $matches[1];
-  $docs[$docid]['doc'] = new_yamlDoc($docid);
+// [ docid=> [ 'doc'=> objet doc, 'ext'=> ext, 'ssdir'=> ssdir, 'catalogs'?=> [ catalogid ], 'shown'?=> 1 ] ]
+$docs = [];
+
+// $docpath est le chemin Unix de la racine des documents
+// $ssdir est le chemin relatif d'un répertoire
+function scan(string $docpath, string $ssdir='') {
+  global $docs;
+  $dirpath = $docpath.($ssdir ? '/'.$ssdir : '');
+  $wd = opendir($dirpath);
+  while (false !== ($entry = readdir($wd))) {
+    //echo "$entry a traiter<br>\n";
+    if (in_array($entry, ['.','..','.git','.htaccess']))
+      continue;
+    elseif (is_dir($docpath.'/'.($ssdir ? "$ssdir/$entry" : $entry)))
+      scan($docpath, $ssdir ? "$ssdir/$entry" : $entry);
+    elseif (preg_match('!^(.*)\.(yaml|php)$!', $entry, $matches)) {
+      $docid = ($ssdir ? $ssdir.'/' : '').$matches[1];
+      $docs[$docid]['doc'] = new_yamlDoc($docid);
+      if (!$docs[$docid]['doc'])
+        echo "Erreur new_yamlDoc($docid)<br>\n";
+      $docs[$docid]['ssdir'] = $ssdir;
+      $docs[$docid]['ext'] = $matches[2];
+      $docs[$docid]['size'] = filesize($docpath.'/'.($ssdir ? "$ssdir/" : '').$entry);
+    }
+    else
+      echo "$entry non traite<br>\n";
+  }
+  closedir($wd);
 }
-closedir($wd);
+scan('docs');
 
 foreach ($docs as $docid => $doc) {
   if (in_array(get_class($doc['doc']),['YamlCatalog','YamlHomeCatalog'])) {
+    $ssdir = ($doc['ssdir'] ? $doc['ssdir'].'/' : '');
     foreach ($doc['doc']->contents as $childId => $d) {
-      if (isset($docs[$childId])) {
-        if (isset($docs[$childId]['catalogs']))
-          $docs[$childId]['catalogs'][] = $docid;
+      if (isset($docs[$ssdir.$childId])) {
+        if (isset($docs[$ssdir.$childId]['catalogs']))
+          $docs[$ssdir.$childId]['catalogs'][] = $docid;
         else
-          $docs[$childId]['catalogs'] = [ $docid ];
+          $docs[$ssdir.$childId]['catalogs'] = [ $docid ];
       }
     }
   }
@@ -49,6 +70,8 @@ function show(string $id, string $title=null) {
   $doc = $docs[$id];
   if ($doc['doc']->title)
     $title = $doc['doc']->title;
+  elseif (isset($doc['doc']->phpDoc['title']))
+    $title = $doc['doc']->phpDoc['title'];
   $nbcat = isset($doc['catalogs']) ? count($doc['catalogs']) : 0;
   echo "<li>$title ($id) [$nbcat]\n";
   if (isset($doc['shown'])) {
@@ -69,19 +92,19 @@ function show(string $id, string $title=null) {
 }
 
 if (!isset($_GET['action'])) {
-  echo "<ul>";
+  echo "<b>Arborescences</b>:<ul>\n";
   foreach ($docs as $id => $doc) {
     if (!isset($doc['catalogs']))
       show($id);
   }
-  echo "</ul>";
+  echo "</ul>\n";
 
   echo "<b>Notes:</b><ul>
     <li>les titres ou noms en gras ne correspondent à aucun fichier
     <li>le nombre entre crochets est le nombre de fois où le document apparait dans un catalogue
   </ul>";
 }
-else if ($_GET['action']=='prot') {
+elseif ($_GET['action']=='prot') {
   // affichage de la liste des docs protégés et de leur protection
   echo "<ul>";
   foreach ($docs as $id => $doc) {
@@ -94,10 +117,27 @@ else if ($_GET['action']=='prot') {
   }
   echo "</ul>";
 }
-else
+elseif ($_GET['action']=='stats') {
+  $countPerExt = [];
+  $sizePerExt = [];
+  foreach ($docs as $id => $doc) {
+    if (!isset($countPerExt[$doc['ext']])) {
+      $countPerExt[$doc['ext']] = 0;
+      $sizePerExt[$doc['ext']] = 0;
+    }
+    $countPerExt[$doc['ext']]++;
+    $sizePerExt[$doc['ext']] += $doc['size'];
+  }
+  echo count($docs)," documents<br>\n";
+  echo "countPerExt = "; print_r($countPerExt); echo "<br>\n";
+  echo "sizePerExt = "; print_r($sizePerExt); echo "<br>\n";
+}
+else {
   echo "Erreur action inconnue<br>\n";
+}
 
 echo "<b>Menu:</b><br><ul>
   <li><a href='?'>consulter les arborescences de documents</a>
   <li><a href='?action=prot'>consulter les protections</a>
+  <li><a href='?action=stats'>calculer les stats</a>
 </ul>\n";
