3130df6a76a7fe6a6d04e889d264253f3d1fc3d3
Benoit DAVID <benoit.davi@free.fr>
2018-07-07 21:25:21 +0000

    add mkot.php

---
+++ docinspire/mkot.php
@@ -0,0 +1,16 @@
+<?php
+// mkot.php - fabrique les objecttypes de inspire-datamodel.yaml
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+if (php_sapi_name()<>'cli')
+  echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>mkscheme</title></head><body><pre>\n";
+
+$ot = [];
+foreach(['sot','datatype','externaltype','unknowntype','uniontype'] as $mttag) {
+  $ot = array_merge($ot, unserialize(file_get_contents("$mttag.pser")));
+}
+echo Yaml::dump(['objectTypes'=> $ot], 999, 2);
