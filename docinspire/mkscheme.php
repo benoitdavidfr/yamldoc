defc810dffc63f13d7754a7083a55e8b96eb343d
Benoit DAVID <benoit.davi@free.fr>
2018-07-07 21:42:34 +0000

    modifs

--- docinspire/mkscheme.php
+++ docinspire/mkscheme.php
@@ -6,16 +6,15 @@ require_once __DIR__.'/../../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
 
-echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>mkscheme</title></head><body>\n";
+if (php_sapi_name()<>'cli')
+  echo "<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>mkscheme</title></head><body><pre>\n";
 
 $codelists = unserialize(file_get_contents('codelist.pser'));
 $enums = unserialize(file_get_contents('enum.pser'));
 //echo "<pre>",Yaml::dump($enums, 999, 2),"</pre>\n"; die();
 
-echo "<pre>",
-      Yaml::dump(
+echo Yaml::dump(
         [ 'schemes'=> array_merge($codelists['schemes'], $enums['schemes']),
           'concepts'=> array_merge($codelists['concepts'], $enums['concepts']),
         ],
-        999, 2),
-      "</pre>\n";
+        999, 2);
